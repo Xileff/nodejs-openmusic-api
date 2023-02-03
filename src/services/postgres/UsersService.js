2df1c8e9291b2bb7d18039ebb1b0f4020307911d
Felix <felixsavero78@gmail.com>
2023-02-03 14:31:54 +0700

    minor bug fix

--- src/services/postgres/UsersService.js
+++ src/services/postgres/UsersService.js
@@ -1,5 +1,6 @@
 const { nanoid } = require('nanoid');
 const { Pool } = require('pg');
+const bcrypt = require('bcrypt');
 const InvariantError = require('../../exceptions/InvariantError');
 
 class UsersService {
@@ -8,19 +9,14 @@ class UsersService {
   }
 
   async addUser({ username, password, fullname }) {
-    const queryCheck = {
-      text: 'SELECT username FROM users WHERE username = $1',
-      values: [username],
-    };
-    const existingUser = await this._pool.query(queryCheck);
-    if (existingUser.rowCount) {
-      throw new InvariantError('Username sudah digunakan');
-    }
+    await this.verifyNewUsername(username);
 
-    const userId = `user-${nanoid(16)}`;
+    const id = `user-${nanoid(16)}`;
+    const hashedPassword = await bcrypt.hash(password, 10);
+
     const queryInsert = {
       text: 'INSERT INTO users VALUES ($1, $2, $3, $4) RETURNING id;',
-      values: [userId, username, password, fullname],
+      values: [id, username, hashedPassword, fullname],
     };
 
     const { rows } = await this._pool.query(queryInsert);
@@ -30,6 +26,18 @@ class UsersService {
     }
 
     return rows[0].id;
+  }
+
+  async verifyNewUsername(username) {
+    const query = {
+      text: 'SELECT username FROM users WHERE username = $1',
+      values: [username],
+    };
+
+    const result = await this._pool.query(query);
+    if (result.rowCount) {
+      throw new InvariantError('Username sudah digunakan');
+    }
   }
 }
 
